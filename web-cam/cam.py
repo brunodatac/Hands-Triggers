0d77f0ee85f3365d8336150be4e55114d5cc69eb
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-03-24 15:43:57 -0300

    add warnings

--- web-cam/cam.py
+++ web-cam/cam.py
@@ -1,10 +1,17 @@
 import cv2
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Webcam error")
 
 while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
     
-    cv2.imshow("Imagem", frame)
+    if not ret:
+        print("Not Stream")
+        break
+    
+    cv2.imshow("Image", frame)
     
     cv2.waitKey(1)
