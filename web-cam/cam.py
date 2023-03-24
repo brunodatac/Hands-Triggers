1e3721a9167c37ee9d9c1f9ebf17b0311c46f1d3
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-03-24 16:01:23 -0300

    add function webcam in cam.py

--- web-cam/cam.py
+++ web-cam/cam.py
@@ -1,18 +1,30 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+def webcam(cam_number):
+    cap = cv2.VideoCapture(cam_number)
+    window_name = "Webcam"
 
-if not cap.isOpened():
-    print("Webcam error")
-    exit()
+    if not cap.isOpened():
+        print("Webcam error")
+        exit()
 
-while True:
-    ret, frame = cap.read()
-    
-    if not ret:
-        print("Not Stream")
-        break
-    
-    cv2.imshow("Image", frame)
-    
-    cv2.waitKey(1)
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("Not Stream")
+            break
+        
+        cv2.imshow(window_name, frame)
+        
+        k = cv2.waitKey(1)
+        
+        if k == ord('q'):
+            break
+        
+        if cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
+            break
+        
+    cv2.detroyAllWindows()
+    cap.release()
+    print("Close")
