bc4abc456db34258a7a9f0f86798e0ad92bfc0cc
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-04-01 13:27:27 -0300

    cam test

--- hands_triggers.py
+++ hands_triggers.py
@@ -3,4 +3,4 @@ from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
 model = YOLO(r"D:\Projetos\Hand-Signals\train\best_.pt")
-model.predict(source="0", show=True, conf=0.5)
+model.predict(source="0", show=True, conf=0.25)
