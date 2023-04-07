4d0cd6f5d2bbf7072b8a95a3bb3d5fd44ecb4e38
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-04-07 13:07:02 -0300

    config conf

--- hands_triggers.py
+++ hands_triggers.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
-model = YOLO(r"D:\Projetos\Hand-Signals\train\best_n_50e.pt")
+model = YOLO(r"D:\Projetos\Hand-Signals\train\best_n_50e_8class.pt")
 model.predict(source="0", show=True, conf=0.40)
