2b24dfe77d2fb823bfe78aac76e4586fdc302fc6
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-04-08 16:40:50 -0300

    feat: opencv integration

--- hands_triggers.py
+++ hands_triggers.py
@@ -1,6 +1,27 @@
 from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 import cv2
 
 model = YOLO(r"D:\Projetos\Hand-Signals\train\best_n_50e_8class.pt")
-model.predict(source="0", show=True, conf=0.40)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    
+    # Lendo cada frame do vídeo
+    ret, img = cap.read()
+    if not ret:
+        break
+    
+    # Executando a predição com o modelo YOLO
+    results = model.predict(img,  conf=0.40, classes=21, show=False)
+
+
+    # Exibindo a imagem na janela
+    cv2.imshow("YOLOv8", img)
+    
+    # Aguardando a tecla 'q' para finalizar a execução
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
