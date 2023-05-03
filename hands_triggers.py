7ca690fcb3d8dae556417b4786f762ec6d56e3aa
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-03 18:59:09 -0300

    feat: voice_search

--- hands_triggers.py
+++ hands_triggers.py
@@ -1,14 +1,26 @@
-from ultralytics import YOLO
+import os
 import subprocess
+import threading
 import time
+
+from ultralytics import YOLO
 import torch
 import cv2
 
-model = YOLO(r"D:\Projetos\Hand-Signals\train\best_cuda.pt")
+
+model = YOLO(r"D:\Projetos\Hand-Signals\train\best_low_res.pt")
 
 cap = cv2.VideoCapture(0)
 
+
 cont = 0
+# thread_voice_search = None
+
+# def executa_voice_search():
+#     global processo  # adiciona essa linha para poder acessar o processo fora da função
+#     os.chdir(os.path.dirname(__file__))
+#     processo = subprocess.Popen(['python', 'voice_search.py'], stdin=subprocess.PIPE)
+
 
 while True:
     
@@ -19,18 +31,30 @@ while True:
 
     with torch.no_grad():
         # Executando a predição com o modelo YOLO
-        results_v = model.predict(img,  conf=0.65, classes=21, show=True)
-        results_w = model.predict(img,  conf=0.65, classes=22, show=True)
-    
+        results_v = model.predict(img,  conf=0.65, classes=21, show=False)
+        results_w = model.predict(img,  conf=0.65, classes=22, show=False)
+        
     for result in results_v:
         #box.append(result.boxes.cls)
         for tensor in result.boxes.cls:
             if torch.allclose(tensor, torch.tensor([21.])):
                 cont += 1
                 if cont == 1:
-                    processo = subprocess.Popen(['notepad.exe', 'D:\Projetos\Teste\meabra.txt'])
+                    processo = subprocess.Popen(['python', 'voice_search.py'])
                     pass
-                    #processo.wait()
+                    processo.wait()
+                    cont = 0
+                    # processo.wait()
+    
+    # pra realizar o processo de multiplos scritps ao mesmo tempo
+    # for result in results_v:
+    #     #box.append(result.boxes.cls)
+    #     for tensor in result.boxes.cls:
+    #         if torch.allclose(tensor, torch.tensor([21.])):
+    #             cont += 1
+    #             if cont == 1:
+    #                 thread_voice_search = threading.Thread(target=executa_voice_search)
+    #                 thread_voice_search.start()
 
     for result_w in results_w:
         #box.append(result.boxes.cls)
@@ -39,16 +63,10 @@ while True:
                 processo.terminate() # fecha o programa
                 cont = 0
                 break
-
-
-    # # Exibindo a imagem na janela
-    # cv2.imshow("YOLOv8", img)
-    
-    # Aguardando a tecla 'q' para finalizar a execução
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-#print(box)
+            
+    # # Aguardando a tecla 'q' para finalizar a execução
+    # if cv2.waitKey(1) == ord('q'):
+    #     break
 
 cap.release()
 cv2.destroyAllWindows()
