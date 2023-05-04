bdde9fb97edfdd632f575db67de37862da5e3043
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-04 15:39:39 -0300

    feat: control_trigger

--- hands_triggers.py
+++ hands_triggers.py
@@ -10,17 +10,12 @@ import cv2
 
 model = YOLO(r"D:\Projetos\Hand-Signals\train\best_low_res.pt")
 
+control_file = 'D:\projetos\Hand-Signals\stop_voice_search.txt'
+
 cap = cv2.VideoCapture(0)
 
 
 cont = 0
-# thread_voice_search = None
-
-# def executa_voice_search():
-#     global processo  # adiciona essa linha para poder acessar o processo fora da função
-#     os.chdir(os.path.dirname(__file__))
-#     processo = subprocess.Popen(['python', 'voice_search.py'], stdin=subprocess.PIPE)
-
 
 while True:
     
@@ -32,7 +27,7 @@ while True:
     with torch.no_grad():
         # Executando a predição com o modelo YOLO
         results_v = model.predict(img,  conf=0.65, classes=21, show=False)
-        results_w = model.predict(img,  conf=0.65, classes=22, show=False)
+        results_l = model.predict(img,  conf=0.65, classes=11, show=False)
         
     for result in results_v:
         #box.append(result.boxes.cls)
@@ -40,33 +35,24 @@ while True:
             if torch.allclose(tensor, torch.tensor([21.])):
                 cont += 1
                 if cont == 1:
+                    if os.path.exists(control_file):
+                        # Remove o arquivo de controle e encerra a execução do script
+                        os.remove(control_file)
+                        
                     processo = subprocess.Popen(['python', 'voice_search.py'])
                     pass
-                    processo.wait()
-                    cont = 0
-                    # processo.wait()
     
-    # pra realizar o processo de multiplos scritps ao mesmo tempo
-    # for result in results_v:
-    #     #box.append(result.boxes.cls)
-    #     for tensor in result.boxes.cls:
-    #         if torch.allclose(tensor, torch.tensor([21.])):
-    #             cont += 1
-    #             if cont == 1:
-    #                 thread_voice_search = threading.Thread(target=executa_voice_search)
-    #                 thread_voice_search.start()
 
-    for result_w in results_w:
+    for result_l in results_l:
         #box.append(result.boxes.cls)
-        for tensor_w in result_w.boxes.cls:
-            if torch.allclose(tensor_w, torch.tensor([22.])):
-                processo.terminate() # fecha o programa
+        for tensor_l in result_l.boxes.cls:
+            if torch.allclose(tensor_l, torch.tensor([11.])):
+                with open("stop_voice_search.txt", "w") as f:
+                    f.write("Encerrar script")
+                processo.terminate()
                 cont = 0
                 break
             
-    # # Aguardando a tecla 'q' para finalizar a execução
-    # if cv2.waitKey(1) == ord('q'):
-    #     break
 
 cap.release()
 cv2.destroyAllWindows()
--- voice_search.py
+++ voice_search.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import os
+
+# Define o nome do arquivo de controle que será usado para encerrar o script
+control_file = 'D:\projetos\Hand-Signals\stop_voice_search.txt'
 
 # Cria uma instância do driver do navegador com as configurações necessárias
 options = webdriver.ChromeOptions()
@@ -12,8 +16,34 @@ driver.maximize_window()
 # Navega até a página do Google
 driver.get('https://www.google.com.br')
 time.sleep(1.5)
+
 # Início da busca por voz
 voice_search_button = driver.find_element(By.XPATH, '//div[@aria-label="Pesquisar por voz"]')
 voice_search_button.click()
-time.sleep(20)
 
+# Espera até o botão de "Ouvindo" desaparecer
+while True:
+    try:
+        driver.find_element(By.XPATH, '//div[contains(text(), "Ouvindo...")]')
+        time.sleep(1)
+    except:
+        break
+
+# Espera até que a página de resultados seja carregada completamente
+while True:
+    try:
+        driver.find_element(By.XPATH, '//div[@id="search"]')
+        break
+    except:
+        time.sleep(1)
+
+# Loop para manter a página de resultados aberta até receber um comando de outro script para encerrar
+while True:
+    # Verifica se o arquivo de controle existe
+    if os.path.exists(control_file):
+        # Remove o arquivo de controle e encerra a execução do script
+        driver.quit()
+        os.remove(control_file)
+    else:
+        # Aguarda 60 segundos antes de verificar novamente
+        time.sleep(20)
