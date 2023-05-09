f6ddc3d70644e58d4a67e15df7bf701b3ad649ed
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-09 12:33:22 -0300

    feat: voice search open and close func

--- hands_triggers.py
+++ hands_triggers.py
@@ -7,15 +7,16 @@ from ultralytics import YOLO
 import torch
 import cv2
 
+from voice_search import Voice
 
 model = YOLO(r"D:\Projetos\Hand-Signals\train\best_low_res.pt")
 
-control_file = 'D:\projetos\Hand-Signals\stop_voice_search.txt'
 
 cap = cv2.VideoCapture(0)
 
 
 cont = 0
+
 
 while True:
     
@@ -23,11 +24,11 @@ while True:
     ret, img = cap.read()
     if not ret:
         break
-
-    with torch.no_grad():
-        # Executando a predição com o modelo YOLO
-        results_v = model.predict(img,  conf=0.65, classes=21, show=False)
-        results_l = model.predict(img,  conf=0.65, classes=11, show=False)
+        
+    # Executando a predição com o modelo YOLO
+    time.sleep(0.5)
+    results_v = model.predict(img,  conf=0.65, classes=21, show=False)
+    results_l = model.predict(img,  conf=0.65, classes=11, show=False)
         
     for result in results_v:
         #box.append(result.boxes.cls)
@@ -35,11 +36,8 @@ while True:
             if torch.allclose(tensor, torch.tensor([21.])):
                 cont += 1
                 if cont == 1:
-                    if os.path.exists(control_file):
-                        # Remove o arquivo de controle e encerra a execução do script
-                        os.remove(control_file)
-                        
-                    processo = subprocess.Popen(['python', 'voice_search.py'])
+                    processo = Voice()
+                    processo.abrir()
                     pass
     
 
@@ -47,12 +45,8 @@ while True:
         #box.append(result.boxes.cls)
         for tensor_l in result_l.boxes.cls:
             if torch.allclose(tensor_l, torch.tensor([11.])):
-                with open("stop_voice_search.txt", "w") as f:
-                    f.write("Encerrar script")
-                processo.terminate()
-                cont = 0
-                break
-            
-
-cap.release()
-cv2.destroyAllWindows()
+                if cont >= 1:
+                    processo.fechar('y')
+                    #processo.terminate()
+                    cont = 0
+                    pass
