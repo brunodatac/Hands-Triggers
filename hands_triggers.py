ab754988ff9c8827a4f398e568bc18e1c5fa8f72
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-17 11:20:00 -0300

    feat: display process bar

--- hands_triggers.py
+++ hands_triggers.py
@@ -9,6 +9,8 @@ import cv2
 
 from voice_search import Voice
 from media_controller import Controller
+from popup import Display
+
 
 
 def search(n_classe):
@@ -17,16 +19,17 @@ def search(n_classe):
     if torch.allclose(n_classe, torch.tensor([21.])): # classe: V | pesquisa de voz
         if not pesquisa_executando:
             pesquisa_executando = True
+            Display.processbar("Abrindo pesquisa...")
             processo = Voice()
             processo.abrir()
+            
 
     elif torch.allclose(n_classe, torch.tensor([11.])): # classe: L | fechar pesquisa de voz
         if pesquisa_executando:
             pesquisa_executando = False
+            Display.processbar("Encerrando...")
             processo.fechar('y')
-    else:
-        return "Invalid case"
-    
+
 
 def media_control(n_classe):
     controller = Controller()
@@ -47,17 +50,18 @@ cap = cv2.VideoCapture(0)
 pesquisa_executando = False
 processo = None
 
+
 while True:
     
     # Lendo cada frame do vídeo
     ret, img = cap.read()
     if not ret:
         break
-        
+
     # Executando a predição com o modelo YOLO
     time.sleep(0.5)
-    results = model.predict(img,  conf=0.80, show=False)
-        
+    results = model.predict(img,  conf=0.90, show=False)
+
     for result in results:
         #box.append(result.boxes.cls)
         for tensor in result.boxes.cls:
