a8433ae72d8d88f29bc4e6c3166bc0b70914c572
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-17 16:17:41 -0300

    feat: process bar

--- media_controller.py
+++ media_controller.py
@@ -2,6 +2,8 @@ import pyautogui
 import torch
 import time
 
+from popup import Display
+
 class Controller():
     
     def playpause(self):
@@ -17,13 +19,16 @@ class Controller():
         pyautogui.press('prevtrack')
         time.sleep(1.5)
 
-    def media_control(self, n_classe):
-        
+    def media_control(self, n_classe):        
         if torch.allclose(n_classe, torch.tensor([14.])): # classe: O | play/pause
+            Display.processbar("PLAY/PAUSE", 0.3)
             self.playpause()
 
         elif torch.allclose(n_classe, torch.tensor([8.])): # classe: I | proxima faixa
+            Display.processbar("NEXT", 0.3)
             self.nexttrack()
         
         elif torch.allclose(n_classe, torch.tensor([22.])): # classe: W | voltar faixa
+            Display.processbar("BACK", 0.3)
             self.prevtrack()
+
