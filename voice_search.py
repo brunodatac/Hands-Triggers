9f1eee41d6c73d01fec1b2b874b02609a653ebaf
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-17 15:11:45 -0300

    feat: func search

--- voice_search.py
+++ voice_search.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import torch
 import time
 import os
 
+from popup import Display
+
 class Voice():
     
-    def __init__(self):
+    def abrir(self):
         # Cria uma instância do driver do navegador com as configurações necessárias
         options = webdriver.ChromeOptions()
         options.add_argument('--use-fake-ui-for-media-stream')  # Adiciona a opção para usar a UI falsa
@@ -17,8 +20,6 @@ class Voice():
         self.driver.get('https://www.google.com.br')
         time.sleep(1.5)
 
-
-    def abrir(self):
         # Início da busca por voz
         voice_search_button = self.driver.find_element(By.XPATH, '//div[@aria-label="Pesquisar por voz"]')
         voice_search_button.click()
@@ -45,3 +46,24 @@ class Voice():
         if key == 'y':
             # Remove o arquivo de controle e encerra a execução do script
             self.driver.quit()
+
+
+    def search(self, n_classe):
+        global pesquisa_executando
+
+        if torch.allclose(n_classe, torch.tensor([21.])): # classe: V | pesquisa de voz
+            if not pesquisa_executando:
+                pesquisa_executando = True
+                Display.processbar("Abrindo pesquisa...", 0.005)
+                self.abrir()
+
+
+        elif torch.allclose(n_classe, torch.tensor([11.])): # classe: L | fechar pesquisa de voz
+            if pesquisa_executando:
+                pesquisa_executando = False
+                Display.processbar("Encerrando...", 0.005)
+                self.fechar('y')
+
+
+
+pesquisa_executando = False
