f6b375406c683f1ee9fc7ec89dc8bc6777fba51a
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-17 14:43:35 -0300

    feat: func media_control

--- media_controller.py
+++ media_controller.py
@@ -1,4 +1,5 @@
 import pyautogui
+import torch
 import time
 
 class Controller():
@@ -15,3 +16,14 @@ class Controller():
         pyautogui.press('prevtrack')
         pyautogui.press('prevtrack')
         time.sleep(1.5)
+
+    def media_control(self, n_classe):
+        
+        if torch.allclose(n_classe, torch.tensor([14.])): # classe: O | play/pause
+            self.playpause()
+
+        elif torch.allclose(n_classe, torch.tensor([8.])): # classe: I | proxima faixa
+            self.nexttrack()
+        
+        elif torch.allclose(n_classe, torch.tensor([22.])): # classe: W | voltar faixa
+            self.prevtrack()
