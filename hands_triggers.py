17f0726f89e1308ecdfe25c76f10007478d4d434
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-17 15:11:31 -0300

    refactor: remove func search

--- hands_triggers.py
+++ hands_triggers.py
@@ -1,10 +1,5 @@
-import os
-import subprocess
-import threading
-import time
-
 from ultralytics import YOLO
-import torch
+import time
 import cv2
 
 from voice_search import Voice
@@ -13,31 +8,15 @@ from popup import Display
 
 
 
-def search(n_classe):
-    global processo, pesquisa_executando
-
-    if torch.allclose(n_classe, torch.tensor([21.])): # classe: V | pesquisa de voz
-        if not pesquisa_executando:
-            pesquisa_executando = True
-            Display.processbar("Abrindo pesquisa...", 0.005)
-            processo = Voice()
-            processo.abrir()
-            
-
-    elif torch.allclose(n_classe, torch.tensor([11.])): # classe: L | fechar pesquisa de voz
-        if pesquisa_executando:
-            pesquisa_executando = False
-            Display.processbar("Encerrando...", 0.005)
-            processo.fechar('y')
-
-
-
 model = YOLO(r"D:\Projetos\Hand-Signals\train\best_low_res.pt")
 cap = cv2.VideoCapture(0)
+voice = Voice()
 controller = Controller()
 pesquisa_executando = False
 processo = None
 Display.processbar("Iniciando...", 0.1)
+
+
 
 while True:
     
@@ -53,5 +32,5 @@ while True:
     for result in results:
         #box.append(result.boxes.cls)
         for tensor in result.boxes.cls:
-            search(tensor)
+            voice.search(tensor)
             controller.media_control(tensor)
