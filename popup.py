0a5843a666e5b6ca2257ce3111208c54bc94eb61
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-17 14:44:15 -0300

    feat: dinamic process bar

--- popup.py
+++ popup.py
@@ -1,17 +1,41 @@
 import PySimpleGUI as sg
 import time
 
-
 class Display():
     
-    def processbar(texto):
-        layout = [[sg.Text(texto, key='-TEXT-')],
-                [sg.ProgressBar(1000, orientation='h', size=(20, 20), key='-PROGRESSBAR-')]]
+    def processbar(texto, ms):
+        layout = [[sg.Text(texto),
+                 sg.ProgressBar(1000, orientation='h', size=(20, 20), key='-PROGRESSBAR-')]]
         
         window = sg.Window('', layout, no_titlebar=True, finalize=True)
         
         for i in range(1000):
-            time.sleep(0.005/1000)
+            time.sleep(ms/1000)
             window['-PROGRESSBAR-'].update_bar(i + 1)
             
         window.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
