8af0d54d2e12d6930eda29ab179e55d3c7179c4b
Bruno Rodrigues <brunorb.dev@gmail.com>
2023-05-19 16:56:09 -0300

    change classes to control media

--- features/media_controller.py
+++ features/media_controller.py
@@ -20,15 +20,15 @@ class Controller():
         time.sleep(1.5)
 
     def media_control(self, n_classe):        
-        if torch.allclose(n_classe, torch.tensor([14.])): # classe: O | play/pause
+        if torch.allclose(n_classe, torch.tensor([1.])): # classe: B | play/pause
             Display.processbar("PLAY/PAUSE", 0.15)
             self.playpause()
 
-        elif torch.allclose(n_classe, torch.tensor([8.])): # classe: I | proxima faixa
+        elif torch.allclose(n_classe, torch.tensor([24.])): # classe: Y | proxima faixa
             Display.processbar("NEXT", 0.15)
             self.nexttrack()
         
-        elif torch.allclose(n_classe, torch.tensor([22.])): # classe: W | voltar faixa
+        elif torch.allclose(n_classe, torch.tensor([11.])): # classe: L | voltar faixa
             Display.processbar("BACK", 0.15)
             self.prevtrack()
 
